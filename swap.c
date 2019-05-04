2d91fb1719ddba1cf6dd306cc080635cb19e5e34
Joseph Abero <joseph.abero@yahoo.com>
2019-05-04 15:51:01 -0700

    working parse

--- swap.c
+++ swap.c
@@ -3,21 +3,28 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct process{
+struct process
+{
   int process_id;
   char action;
   int page;
   int virtualpage; //??
   int physicalpage; //??
-}
+};
 
 void getMemoryList(struct process process_list[], int *num_of_actions);
 
 int main(){
-  struct process process_list[100];
-  int num_of_actions;
+    struct process process_list[100];
+    int num_of_actions;
   
-  getMemoryList(process_list, &num_of_actions);
+    getMemoryList(process_list, &num_of_actions);
+
+    for(int i = 0; i < num_of_actions; i++)
+    {
+          printf("PROCESS #%i: process_id: %i action: %c page: %i\n", 
+              i + 1, process_list[i].process_id, process_list[i].action, process_list[i].page);
+    }
 }
 
 void getMemoryList(struct process process_list[], int *num_of_actions)
@@ -33,14 +40,19 @@ void getMemoryList(struct process process_list[], int *num_of_actions)
     }
 
     char line[1000];
-    int process_id, page;
-    char action;
+    int process_id, page, action;
     int n, index = 0;
 
     while(fgets(line, 1000, fp) != NULL)
     {
-        n = sscanf(line, "%i %i %i\n", &process_id, &action, &page);
-        if(n > 0)
+        n = sscanf(line, "%i %lc %i\n", &process_id, &action, &page);
+        if(n == 2)
+        {
+            process_list[index].process_id = process_id;
+            process_list[index].action = action;
+            process_list[index].page = 0;
+        }
+        else
         {
             process_list[index].process_id = process_id;
             process_list[index].action = action;
