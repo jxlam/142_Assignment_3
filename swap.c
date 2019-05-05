b83de9661bd81ee5cc656a97f0fa7b016e601216
Joseph Abero <joseph.abero@yahoo.com>
2019-05-04 17:45:56 -0700

    working created page. need more time to setup

--- swap.c
+++ swap.c
@@ -5,51 +5,62 @@
 
 typedef enum
 {
-    READY,
-    KILLED
+    READY = 0,
+    KILLED = 1
 } STATUS;
+
+struct command
+{
+    int         command_id;
+    char        action;
+    int         page;
+};
 
 struct process
 {
     int         process_id;
-    char        action;
-    int         page;
     int         virtual_addr;
     int         physical_addr;
     int         dirty;
     STATUS      status;
 };
 
-void getMemoryList(struct process process_list[], int *num_of_actions);
+void getCommandList(struct command command_list[], int *num_of_commands);
 
-void FIFO_MEM(struct process process_list[], int *num_of_actions);
-void LRU_MEM(struct process process_list[], int *num_of_actions);
-void RANDOM_MEM(struct process process_list[], int *num_of_actions);
+void FIFO_MEM(struct command command_list[], int *num_of_commands);
+void LRU_MEM(struct command command_list[], int *num_of_commands);
+void RANDOM_MEM(struct command command_list[], int *num_of_commands);
 
-int chooseAction(char action);
-void createPage();
-void terminatePage();
-void allocatePage();
-void readPage();
-void writePage();
-void freePage();
+int executeAction(struct command command, struct process* process_list, int* physical_space, int* swap_space, int* process_index, int* swap_index, int* physical_index);
+int createProcess(struct process* process_list, int* process_index, int process_id);
+void terminateProcess();
+void allocateProcess();
+void readProcess();
+void writeProcess();
+void freeProcess();
+
+void printCommand(struct command Command);
+void printProcess(struct process Process);
 
 
 int main(){
-    struct process process_list[100];
-    int num_of_actions;
+    struct command command_list[100];
+    int num_of_commands;
   
-    getMemoryList(process_list, &num_of_actions);
+    getCommandList(command_list, &num_of_commands);
 
-    // for(int i = 0; i < num_of_actions; i++)
-    // {
-    //       printf("PROCESS #%i: process_id: %i action: %c page: %i\n", 
-    //           i + 1, process_list[i].process_id, process_list[i].action, process_list[i].page);
-    // }
-    FIFO_MEM(process_list, &num_of_actions);
+    for(int i = 0; i < num_of_commands; i++)
+    {
+            printCommand(command_list[i]);
+    }
+
+
+    FIFO_MEM(command_list, &num_of_commands);
+    LRU_MEM(command_list, &num_of_commands);
+    RANDOM_MEM(command_list, &num_of_commands);
 }
 
-void getMemoryList(struct process process_list[], int *num_of_actions)
+void getCommandList(struct command command_list[], int *num_of_commands)
 {
     FILE *fp;
 
@@ -61,76 +72,93 @@ void getMemoryList(struct process process_list[], int *num_of_actions)
     }
 
     char line[1000];
-    int process_id, page, action;
+    int command_id, page, action;
     int n, index = 0;
 
     while(fgets(line, 1000, fp) != NULL)
     {
-        n = sscanf(line, "%i %lc %i\n", &process_id, &action, &page);
+        n = sscanf(line, "%i %lc %i\n", &command_id, &action, &page);
 
         if(n > 0)
         {
-            process_list[index].process_id = process_id;
-            process_list[index].action = action;
-            process_list[index].page = page;
-            process_list[index].virtual_addr = -1;
-            process_list[index].physical_addr = -1;
-            process_list[index].dirty = 0;
-            process_list[index].status = READY;
+            command_list[index].command_id = command_id;
+            command_list[index].action = action;
+            command_list[index].page = page;
             if(n == 2)
             {
-                process_list[index].page = 0;
+                command_list[index].page = 0;
             }
         }
         index++;
     }
     fclose(fp);
 
-    *num_of_actions = index;
+    *num_of_commands = index;
 }
 
-void FIFO_MEM(struct process process_list[], int *num_of_actions)
+void FIFO_MEM(struct command command_list[], int *num_of_commands)
 {
+    printf("\n\t\t\t\tFIFO MEM\n");
+    printf("-----------------------------------------------------------------------------------\n");
+
+    struct process process_list[100];
     int physical_space[20];
     int swap_space[1000];
 
-    int physical_index;
-    int swap_index;
+    int process_index = 0;
+    int physical_index = 0;
+    int swap_index = 0;
 
-    for(int i=0; i < *num_of_actions; i++)
+    for(int i=0; i < *num_of_commands; i++)
     {
-        // Execute process command
-        printf("PROCESS #%i: process_id: %i action: %c page: %i\n", 
-              i + 1, process_list[i].process_id, process_list[i].action, process_list[i].page);
-        if(chooseAction(process_list[i].action) == 0)
+        // Execute command command
+        printf("COMMAND #%i: ", i + 1);
+        printCommand(command_list[i]);
+
+        if(executeAction(command_list[i], process_list, physical_space, swap_space, &process_index, &swap_index, &physical_index) == 0)
         {
-            printf("PROCESS COMMAND FAILED\n");
+            printf("COMMAND FAILED\n");
         }
     }
 }
 
-int chooseAction(char action)
+void LRU_MEM(struct command command_list[], int *num_of_commands)
+{
+    printf("\n\t\t\t\tLRU MEM\n");
+    printf("-----------------------------------------------------------------------------------\n");
+
+}
+
+void RANDOM_MEM(struct command command_list[], int *num_of_commands)
+{
+    printf("\n\t\t\t\tRANDOM MEM\n");
+    printf("-----------------------------------------------------------------------------------\n");
+
+}
+
+int executeAction(struct command command, struct process* process_list, int* physical_space, int* swap_space, int* process_index, int* swap_index, int* physical_index)
 {
     int result = 1;
-    switch(action)
+    switch(command.action)
     {
         case 'C':   
-            createPage();
+            createProcess(process_list, process_index, command.command_id);
+            printProcess(process_list[(*process_index) - 1]);
             break;
         case 'T':   
-            terminatePage();
+            terminateProcess();
             break;
         case 'A':   
-            allocatePage();
+            allocateProcess();
             break;
         case 'R':   
-            readPage();
+            readProcess();
             break;
         case 'W':   
-            writePage();
+            writeProcess();
             break;
         case 'F':   
-            freePage();
+            freeProcess();
             break;
         default:    
             result = 0;
@@ -138,27 +166,52 @@ int chooseAction(char action)
     }
 }
 
-void createPage()
+int createProcess(struct process* process_list, int* process_index, int process_id)
 {
-    printf("createPage\n");
+    printf("createProcess\n");
+    struct process createdProcess;
+
+    createdProcess.process_id       = process_id;
+    createdProcess.virtual_addr     = -1;
+    createdProcess.physical_addr    = -1;
+    createdProcess.dirty            = 0;
+    createdProcess.status           = READY;
+
+    printProcess(createdProcess);
+
+    process_list[(*process_index)++] = createdProcess;
+
+    return 1;
 }
-void terminatePage()
+void terminateProcess()
 {
-    printf("terminatePage\n");
+    printf("terminateProcess\n");
 }
-void allocatePage()
+void allocateProcess()
 {
-    printf("allocatePage\n");
+    printf("allocateProcess\n");
 }
-void readPage()
+void readProcess()
 {
-    printf("readPage\n");
+    printf("readProcess\n");
 }
-void writePage()
+void writeProcess()
 {
-    printf("writePage\n");
+    printf("writeProcess\n");
 }
-void freePage()
+void freeProcess()
 {
-    printf("freePage\n");
+    printf("freeProcess\n");
+}
+
+void printCommand(struct command Command)
+{
+      printf("command_id: %i action: %c page: %i\n", 
+              Command.command_id, Command.action, Command.page);
+}
+
+void printProcess(struct process Process)
+{
+    printf("process_id: %i virtual_addr: %i physical_addr: %i dirty: %i status: %i\n", 
+              Process.process_id, Process.virtual_addr, Process.physical_addr, Process.dirty, Process.status);
 }
